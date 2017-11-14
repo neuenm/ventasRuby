854c678a1d0bed6df70ee76d423c92ddeaa16a10
neuenm <neuenmobilia@gmail.com>
2017-11-14 20:17:19 -0300

    Administracion de productos

--- pedidos.rb
+++ pedidos.rb
@@ -26,20 +26,22 @@ class Producto
 	property :nombre, String
 	property :precio , String
 	property :cantidad, Integer
+	property :categoria, String
 
 	has n, :ventas , :through => Resource
 end
 Venta.auto_upgrade!
 Producto.auto_upgrade!
 
-
-
-
+/DataMapper.auto_migrate!/
+/DataMapper.auto_upgrade!/
 
 get "/" do 
 	slim :layout
 end
+
 get "/nuevo" do
+	@Productos= Producto.all();
 	slim :ventaNueva
 end
 
@@ -49,7 +51,7 @@ post "/nuevo" do
 	j=params[:nombre].length
 	venta = Venta.new(:cliente=>params[:cliente], :fecha=>DateTime.now.to_date, :total=>params[:total])
 	while i<j
-		producto = Producto.new(:nombre=>params[:nombre][i], :precio=>params[:precio][i], :cantidad=>params[:cantidad][i])
+		producto = Producto.new(:nombre=>params[:nombre][i], :precio=>params[:precio][i], :cantidad=>params[:cantidad][i], :categoria=>params[:categoria])
 		venta.productos << producto
 		i += 1
 	end
@@ -64,9 +66,34 @@ post "/nuevo" do
 	end
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
 get "/verhoy" do
 	@listado=Venta.all(:fecha=>DateTime.now.to_date)
 	slim :listado
+end
+
+post "/fecha" do
+	@fecha=params["fecha"]
+	@listado=Venta.all(:fecha=>@fecha)
+	slim  :listado
+end
+
+
+post "/cliente" do
+	@cliente=params["cliente"]
+	@listado=Venta.all(:cliente=>@cliente)
+	slim  :listado
 end
 
 get "/listado" do
@@ -78,11 +105,45 @@ get "/detalle/:id" do
 	@detalle=Venta.get(params['id'])
 	@productos= @detalle.productos
 	@total=@detalle.total
+	p @total
+	p @productos
 	slim :detalle
 end
 
-post "/fecha" do
-	@fecha=params["fecha"]
-	@listado=Venta.all(:fecha=>@fecha)
-	slim  :listado
+
+
+
+get "/listado/productos" do 
+	@productos= Producto.all();
+	slim :listadoProductos	
+end
+
+get "/producto/nuevo" do 
+	slim :nuevoPorducto
+end
+
+
+post "/producto/nuevo" do 
+	producto = Producto.new(:nombre=>params[:nombre], :precio=>params[:precio], :cantidad=>0, :categoria=>params[:categoria]);
+	if producto.save
+		redirect "/listado/productos"
+	else
+		p alfo salio0 mal
+	end
+end
+
+
+
+
+get "/producto/editar/:id" do
+	@producto=Producto.get(params["id"])
+	slim :editaProducto
+end
+
+
+get "/producto/eliminar/:id" do
+	@producto=Producto.get(params["id"])
+	@producto.destroy
+	@productos= Producto.all();
+	slim :listadoProductos	
 end
